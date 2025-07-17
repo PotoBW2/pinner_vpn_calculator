e7e9262051017576d0cbb7e2ddac5202eef399f1
PotoBW <dmosquerabw@gmail.com>
2025-07-17 18:05:38 -0400

    Software terminado entre en fase alfa

--- core.py
+++ core.py
@@ -1,7 +1,9 @@
-from utils import result_ping, id_servidor, id_vpn, crear_vpn, crear_servidor, datos_vpn, datos_servidor, eliminar_ping
+from utils import result_ping, id_servidor, id_vpn, crear_vpn, crear_servidor, datos_vpn, datos_servidor, eliminar_ping_por_fecha, \
+    datos_grafica, nombre_vpn, nombre_servidor
 import keyboard
 import time
 import threading
+import matplotlib.pyplot as plt
 
 print("***************************************************************************************************************")
 print("* Lista de VPNs                                                                                               *")
@@ -36,6 +38,8 @@ while bandera_vpn:
         vpn = id_vpn(selecion_vpn)
         bandera_vpn = False
 print(" ")
+print(" ")
+print(" ")
 print("***************************************************************************************************************")
 print("* Lista de Servidores                                                                                         *")
 print("***************************************************************************************************************")
@@ -69,8 +73,11 @@ while bandera_servidor:
         crear_servidor(selecion_servidor, vpn)
         servidor = id_servidor(selecion_servidor, vpn)
         bandera_servidor = False
+print("  ")
+print("  ")
+print("  ")
 print("***************************************************************************************************************")
-print("* PINEANDO. PARA DETENER PRECIONE F12                                                                    *")
+print("* PINEANDO. PARA DETENER PRECIONE F12                                                                         *")
 print("***************************************************************************************************************")
 condicion_de_parada = True
 
@@ -88,11 +95,15 @@ while True:
     if keyboard.is_pressed('F12'):
         condicion_de_parada = False
         break
+
 bandera = True
 while bandera:
+    print(" ")
+    print(" ")
+    print(" ")
     resp = input("¿Desea mantener los ping obtenidos hoy para este servidor y esta VPN? (s/n): ")
     if resp in ["n", "no", 0, False, None, "N", "No", "nO", "NO"]:
-        eliminar_ping()
+        eliminar_ping_por_fecha()
         print(
             "***************************************************************************************************************")
         print(
@@ -100,9 +111,61 @@ while bandera:
         print(
             "***************************************************************************************************************")
         bandera = False
-        time.sleep(60)
     elif resp in ["s", "si", 1, True, "Si", "SI", "sI", "sí", "S", "Sí", "SÍ", "sÍ", "y", "Y", "Yes", "yes"]:
         bandera = False
+    else:
+        print(
+            "***************************************************************************************************************")
+        print(
+            "*** ERROR: DEBE RESPONDER CORRECTAMENTE LA PREGUNTA (s/n).                                                  ***")
+        print(
+            "***************************************************************************************************************")
+        print(" ")
+        print(" ")
+        print(" ")
+
+nom_vpn = nombre_vpn(vpn)
+nom_servidor = nombre_servidor(servidor,vpn)
+bandera = True
+while bandera:
+    resp = input('¿Desea exportar la grafica del Servidor "'+nom_servidor+'" del VPN "'+nom_vpn+'"? (s/n): ')
+    if resp in ["n", "no", 0, False, None, "N", "No", "nO", "NO"]:
+        print(" ")
+        print(" ")
+        print(" ")
+        print(
+            "***************************************************************************************************************")
+        print(
+            "*** Gracias por usar nuestro software                                    By PotoBW                          ***")
+        print(
+            "***************************************************************************************************************")
+        bandera = False
+    elif resp in ["s", "si", 1, True, "Si", "SI", "sI", "sí", "S", "Sí", "SÍ", "sÍ", "y", "Y", "Yes", "yes"]:
+        bandera = False
+        x, y = datos_grafica(6)
+        plt.figure(figsize=(8, 6))
+        plt.plot(x, y,
+                 marker='o',
+                 linestyle='-',
+                 color='blue',
+                 label='Ping')
+        plt.title('Pings del Servidor "' + nom_servidor + '" del VPN "' + nom_vpn + '"', fontsize=14)
+        plt.xlabel("Horario", fontsize=12)
+        plt.ylabel("Milisegundos", fontsize=12)
+        plt.grid(True, linestyle='--', alpha=0.7)
+        plt.legend()
+        plt.savefig(nom_servidor+'_'+nom_vpn+'.png', dpi=300, bbox_inches='tight')
+        plt.show()
+        print(" ")
+        print(" ")
+        print(" ")
+        print(
+            "***************************************************************************************************************")
+        print(
+            "*** Grafica exportada satisfactoriamente. Gracias por usar nuestro software              By PotoBW          ***")
+        print(
+            "***************************************************************************************************************")
+        time.sleep(60)
     else:
         print(
             "***************************************************************************************************************")
--- utils.py
+++ utils.py
@@ -1,16 +1,25 @@
-import time
+import time as timein
 from ping3 import ping
 import sqlite3
-from datetime import date
+from datetime import date, time, timedelta, datetime
+import os
+import platform
 
 conexion = None
 
 
 def result_ping(url, servidor):
-    hora = time.strftime("%H:%M")
+    hora = timein.strftime("%H:%M")
     fecha = date.today()
     bandera = existe_ping(servidor, hora)
-    if not bandera:
+    if bandera:
+        id_viejo, ping_viejo = obtener_id_ping(servidor, hora)
+        result = ping(url)
+        if result:
+            if result < ping_viejo:
+                eliminar_ping_por_id(id_viejo)
+                crear_ping(servidor, round(result * 1000), hora, fecha)
+    else:
         result = ping(url)
         if result:
             crear_ping(servidor, round(result * 1000), hora, fecha)
@@ -23,7 +32,7 @@ def open_connection():
 
 def id_servidor(servidor_nombre, vpn):
     cursor, conn = open_connection()
-    cursor.execute("SELECT id FROM servidor WHERE nombre = '" + servidor_nombre + "' AND vpn = '" + vpn + "'")
+    cursor.execute("SELECT id FROM servidor WHERE nombre = '" + servidor_nombre + "' AND vpn = " + str(vpn))
     registros = cursor.fetchall()
     if len(registros) > 0:
         for registro in registros:
@@ -100,9 +109,72 @@ def existe_ping(servidor, hora):
     conn.close()
 
 
-def eliminar_ping():
+def eliminar_ping_por_fecha():
     fecha = date.today()
     cursor, conn = open_connection()
     cursor.execute("DELETE FROM ping WHERE fecha = '" + str(fecha) + "'")
+    conn.commit()
+    conn.close()
+
+
+def min_to_number(min):
+    return round(min / 60, 2)
+
+
+def datos_grafica(servidor):
+    x = []
+    y = []
+    hora = time(hour=0, minute=0)
+    fecha_hora = datetime.combine(datetime.today(), hora)
+    bandera = fecha_hora + timedelta(days=1)
+    cursor, conn = open_connection()
+    while fecha_hora < bandera:
+        cursor.execute("SELECT ping FROM ping WHERE servidor =" + str(servidor) + " AND hora='" + str(
+            hora.strftime("%H:%M")) + "'")
+        registros = cursor.fetchall()
+        if len(registros) > 0:
+            for registro in registros:
+                y.append(registro[0])
+        else:
+            y.append(None)
+        x.append(hora.hour + min_to_number(hora.minute))
+        fecha_hora = fecha_hora + timedelta(minutes=1)
+        hora = fecha_hora.time()
+    conn.close()
+    return x, y
+
+
+def nombre_servidor(id, vpn):
+    cursor, conn = open_connection()
+    cursor.execute("SELECT nombre FROM servidor WHERE id = '" + str(id) + "' AND vpn = '" + str(vpn) + "'")
+    registros = cursor.fetchall()
+    for registro in registros:
+        return registro[0]
+    conn.close()
+
+
+def nombre_vpn(id):
+    cursor, conn = open_connection()
+    cursor.execute("SELECT nombre FROM vpn WHERE id = '" + str(id) + "'")
+    registros = cursor.fetchall()
+    for registro in registros:
+        return registro[0]
+    conn.close()
+
+
+def obtener_id_ping(servidor, hora):
+    cursor, conn = open_connection()
+    cursor.execute(
+        "SELECT id,ping FROM ping WHERE servidor =" + str(servidor) + " AND hora='" + hora + "'")
+    registros = cursor.fetchall()
+    for registro in registros:
+        print(registro[0])
+        print(registro[1])
+        return registro[0], registro[1]
+
+
+def eliminar_ping_por_id(id):
+    cursor, conn = open_connection()
+    cursor.execute("DELETE FROM ping WHERE id = " + str(id))
     conn.commit()
     conn.close()
