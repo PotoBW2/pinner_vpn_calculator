e7ab209c9f7faf8760e9bce21557a655d319b43e
PotoBW <dmosquerabw@gmail.com>
2025-07-18 16:53:24 -0400

    pruebas en el 1er dia

--- core.py
+++ core.py
@@ -144,16 +144,17 @@ while bandera:
     elif resp in ["s", "si", 1, True, "Si", "SI", "sI", "sí", "S", "Sí", "SÍ", "sÍ", "y", "Y", "Yes", "yes"]:
         bandera = False
         x, y = datos_grafica(servidor)
-        plt.figure(figsize=(8, 6))
+        plt.figure(figsize=(50, 5))
         plt.plot(x, y,
                  marker='None',
                  linestyle='-',
                  color='blue',
                  label='Ping')
         # plt.xlim(0, 24)
-        plt.xticks([0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24],
-                   ['0:00', '2:00', '4:00', '6:00', '8:00', '10:00', '12:00', '14:00', '16:00', '18:00', '20:00',
-                    '22:00', '24:00'])
+        plt.xticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
+                   ['0:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00', '9:00', '10:00', '11:00',
+                    '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00',
+                    '23:00','24:00'])
         plt.title('Pings del Servidor "' + nom_servidor + '" del VPN "' + nom_vpn + '"', fontsize=14)
         plt.xlabel("Horario", fontsize=12)
         plt.ylabel("Milisegundos", fontsize=12)
